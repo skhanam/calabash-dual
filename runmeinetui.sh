e7b039e649f39bde9b2831cb674bda37a6f79fc8
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-01-27 14:00:16 +0000

    script to run tests from command line

--- runmeinetui.sh
+++ runmeinetui.sh
@@ -5,20 +5,20 @@ BUILD_CONFIG="Debug"
 PROJECT_PATH="features"
 ARCHITECTURE_SELECTED=i386
 APPNAME="meine TUI"
+PROJ_DIR="../meine.tui/build/iphone/meine TUI.xcodeproj"
 
-cd ~/projects/meine.tui/build/iphone
-BUILT_PRODUCTS_DIR=$(xcodebuild ARCHS="${ARCHITECTURE_SELECTED}" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator  -configuration "${BUILD_CONFIG}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*" | xargs -L1 dirname)
+if [ "$1" == "clean" ] ; then
+killall Xcode
+./expect.sh
+open -a Xcode
+sleep 20
+xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_DIR}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator build
+fi
 
-cd /Users/tejasvi.manmatha/projects/meine.tui.ui-automation
+BUILT_PRODUCTS_DIR=$(xcodebuild -project "${PROJ_DIR}" ARCHS="${ARCHITECTURE_SELECTED}" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator  -configuration "${BUILD_CONFIG}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*" | xargs -L1 dirname)
 
 APP_BUNDLE_PATH_VAR="${BUILT_PRODUCTS_DIR}"/"${BUILD_CONFIG}"-iphonesimulator/"${APPNAME}".app
 echo $APP_BUNDLE_PATH_VAR
-
-cd /Users/tejasvi.manmatha/projects/meine.tui.ui-automation
-sleep 1
-
-#APP_BUNDLE_PATH_VAR="/Users/tejasvi.manmatha/Library/Developer/Xcode/DerivedData/meine_TUI-duqvirclbmdgamcsaqhrnzynqpmz/Build/Products/Debug-iphonesimulator/meine TUI.app"
-#echo TESTENV="DE_MT" SDK_VERSION=6.0 BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" cucumber -p -f html -o report.html de_mt_ios features/ -v
 
  echo DEVICE_TARGET=simulator TESTENV=DE_MT TESTENV="DE_MT" BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" cucumber -p de_mt_ios -f html -o report.html features/ -v
  DEVICE_TARGET=simulator TESTENV=DE_MT TESTENV="DE_MT" BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" cucumber -p de_mt_ios -f html -o report.html features/ -v
