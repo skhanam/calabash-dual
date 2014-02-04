57665fc34f599b99ec5a80e1b5cc8f5298ca9d5a
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-02-04 10:31:54 +0000

    updating expect script

--- expect.sh
+++ expect.sh
@@ -2,10 +2,10 @@
 set arg1 [lindex $argv 0]
  
 set timeout 10
-spawn ti clean --project-dir ../$arg1
+spawn ti clean --project-dir $arg1
 expect "Project cleaned successfully"
 
-spawn ti build --platform ios --project-dir ../$arg1 --target-deploy development
+spawn ti build --platform ios --project-dir $arg1 --target-deploy development
 set timeout 60
 expect "Alloy compiler completed successfully"
 expect "Focusing the iOS Simulator"
@@ -15,7 +15,7 @@ send \004
 spawn killall Xcode
 spawn sleep 5
 set timeout 20
-spawn calabash-ios setup ../$arg1/build/iphone/
+spawn calabash-ios setup $arg1/build/iphone/
 expect "Please answer yes (y) or no (n)"  { send "n\r" }
 expect "Default target: meine TUI"  {  send "\r" }
 expect "Setup done"
