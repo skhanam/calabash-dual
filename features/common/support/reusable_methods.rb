b278a2e40a1a2c0fada846e11196d557e878155b
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-02-05 11:52:13 +0000

    Adding script to run android tests

--- features/common/support/reusable_methods.rb
+++ features/common/support/reusable_methods.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'rubyXL'
 require 'date'
 require_relative '../support/application_strings'
---
+++ runandroid.sh
@@ -0,0 +1,8 @@
+if [ "$1" == "clean" ] ; then
+echo "\n\n\nCleaning and building application for android tests...\n\n\n"
+ti clean --project-dir ../meine.tui
+ti build --platform android -b --project-dir ../meine.tui
+cp ../meine.tui/build/android/bin/app-unsigned.apk app.apk
+calabash-android resign app.apk
+fi
+calabash-android  run app.apk -p de_mt_android -v
