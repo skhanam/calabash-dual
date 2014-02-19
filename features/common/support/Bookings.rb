08986caea22ba7997581f0a3e904bc678945c32c
Simon Dina <Simon.Dina@akqa.com>
2014-02-19 15:25:33 +0000

    Fixed Unicode errors

--- features/common/support/Bookings.rb
+++ features/common/support/Bookings.rb
@@ -1,3 +1,6 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 BOOKINGS={"payload" => {
     "userName" => {"firstName" => "Peter", "lastName" => "Pan"},
     "reservationLists" =>
--- features/common/support/env.rb
+++ features/common/support/env.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 require_relative 'page_world'
 require_relative 'application_strings'
 
