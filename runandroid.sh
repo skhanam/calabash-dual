8a5cf61b5bf620c06b131d430c1225d0be461a29
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-02-21 20:31:38 +0000

    updating android  shell script for CI

--- runandroid.sh
+++ runandroid.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
-if [ "$1" == "clean" ] ; then
-echo "\n\n\nCleaning and building application for android tests...\n\n\n"
-ti clean --project-dir ../meine.tui
-ti build --platform android -b --project-dir ../meine.tui
-cp ../meine.tui/build/android/bin/app-unsigned.apk app.apk
-calabash-android resign app.apk
+
+if [ -z "$2" ] ; then
+	echo "Running tests on default directory \n\n"
+	PROJ_NAME="meine.tui"
+else
+	if [ "$2" == "NA" ] ; then
+			PROJ_NAME=$2
+	else
+		echo "Example command\n sh runandroid.sh NA meine.tui @android_test"
+	fi
 fi
-calabash-android  run app.apk -p de_mt_android_jenkins
+
+if [ "$1" == "clean" ] ; then
+	echo "\n\n\nCleaning and building application for android tests...\n\n\n"
+	ti clean --project-dir ../$PROJ_NAME
+	ti build --platform android -b --project-dir ../$PROJ_NAME
+	cp ../$PROJ_NAME/build/android/bin/meine\ TUI.apk  app.apk
+	rm -rf test_servers/
+	calabash-android resign app.apk
+	calabash-android build app.apk
+fi
+
+if [ -z "$3" ] ; then
+	echo "Example command\n sh runandroid.sh NA meine.tui @android_test"
+else
+	calabash-android  run app.apk -p de_mt_android_jenkins --tag $3
+fi
