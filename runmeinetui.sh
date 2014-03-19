3b40c01a33623c1657385568e76009fe72cf61e6
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-19 10:41:14 +0000

    Adding node path to build scripts

--- runmeinetui.sh
+++ runmeinetui.sh
@@ -22,7 +22,7 @@ fi
 
 PROJ_NAME="meine.tui"
 
-cd ../${PROJ_NAME}/;node build.js -B meinetui;cd -
+cd ../${PROJ_NAME}/;/usr/local/bin/node build.js -B meinetui;cd -
 
 ruby update_tiapp.rb $PROJ_NAME
 
