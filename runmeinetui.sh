95cc442b95b71ead3ac2347e88c95dd625906f4c
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-19 13:54:41 +0000

    commenting some commands form runmeinetui

--- runmeinetui.sh
+++ runmeinetui.sh
@@ -2,9 +2,6 @@
 clear
 DATE=`date +%d-%m-%Y-%H-%M`
 
-export PATH=$PATH:/usr/local/bin/node
-export PATH=$PATH:/usr/local/bin/ti
-
 if [ "$#" != "2" ]; then
 	echo "\n\n\n2 ARGUMENTS NEEDED"
 	echo "1) clean(clean project) or NA (for running project without cleaning"
@@ -21,6 +18,7 @@ tagged_test=$2
 fi
 
 PROJ_NAME="meine.tui"
+echo "project name:"${PROJ_NAME}
 
 cd ../${PROJ_NAME}/;/usr/local/bin/node build.js --brand meinetui;cd -
 
@@ -39,20 +37,20 @@ killall Xcode
 cp ./expect.exp ../${PROJ_NAME}/
 cd ../${PROJ_NAME}/
 /usr/bin/expect ./expect.exp
-cd -
-open -a Xcode
-sleep  5
-open ../${PROJ_NAME}/build/iphone/*.xcodeproj
-sleep 30
-xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_LOC}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator build
-cp -r ../meine.tui/i18n/* features/test_data/
-fi
-
-BUILT_PRODUCTS_DIR=$(xcodebuild -project "${PROJ_LOC}" ARCHS="${ARCHITECTURE_SELECTED}" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator  -configuration "${BUILD_CONFIG}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*" | xargs -L1 dirname)
-
-APP_BUNDLE_PATH_VAR="${BUILT_PRODUCTS_DIR}"/"${BUILD_CONFIG}"-iphonesimulator/"${APPNAME}".app
-echo $APP_BUNDLE_PATH_VAR
-
-
-echo BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p de_mt_ios_jenkins features/ --tag $tagged_test -v
-BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p de_mt_ios_jenkins features/  --tag $tagged_test
+#cd -
+#open -a Xcode
+#sleep  5
+#open ../${PROJ_NAME}/build/iphone/*.xcodeproj
+#sleep 30
+#xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_LOC}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator build
+#cp -r ../meine.tui/i18n/* features/test_data/
+#fi
+#
+#BUILT_PRODUCTS_DIR=$(xcodebuild -project "${PROJ_LOC}" ARCHS="${ARCHITECTURE_SELECTED}" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator  -configuration "${BUILD_CONFIG}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*" | xargs -L1 dirname)
+#
+#APP_BUNDLE_PATH_VAR="${BUILT_PRODUCTS_DIR}"/"${BUILD_CONFIG}"-iphonesimulator/"${APPNAME}".app
+#echo $APP_BUNDLE_PATH_VAR
+#
+#
+#echo BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p de_mt_ios_jenkins features/ --tag $tagged_test -v
+#BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p de_mt_ios_jenkins features/  --tag $tagged_test
