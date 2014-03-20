5cb797ba61e2e35f593b732a023e4f46c183ee17
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-20 14:17:08 +0000

    updating path var

--- runprofile.sh
+++ runprofile.sh
@@ -1,15 +1,5 @@
-[[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
- [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
- alias sl='open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
- export JMETER_DIR=/Users/Shared/apache-jmeter-2.11
- export PATH=$PATH:/Users/Shared/apache-jmeter-2.11
- 
- export PS1="\W \$: "
- export PATH=/usr/local/bin:$PATH
- 
- export ANDROID_HOME=/Users/qaautomation/Documents/android/sdk
- export PATH=$ANDROID_HOME/tools:$PATH
- export PATH=$ANDROID_HOME/platform-tools:$PATH
- 
- alias sl='open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app'
- export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+#!/bin/sh
+
+export PATH=/usr/local/bin:$PATH
+ti clean
+echo "cleaned project"
