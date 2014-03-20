914170acc5346469d6ae86e45340bce2ba6df56a
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-20 14:27:49 +0000

    updating run ci

--- runprofile.sh
+++ runprofile.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 export PATH=/usr/local/bin:$PATH
-echo "cleaned project"
-which ti
+
+echo "SETTING PROJECT TO MEINE TUI"
+cd meine.tui
+node build.js meinetui --syncLang -b
+
+echo "CLEANING AND BUILDING PROJECT"
+cd ../meine.tui.ui-automation/
+#bundle install
+rm -f *.png
+rm -f *.html
+calabash-ios sim reset
+sh runmeinetui.sh clean @login1
