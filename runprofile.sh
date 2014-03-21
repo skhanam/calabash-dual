80272f2b3a54e5b95150de0772dfd894420f7449
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-21 10:10:14 +0000

    no message

--- runprofile.sh
+++ runprofile.sh
@@ -16,8 +16,9 @@ rm -f *.html
 source /Users/qaautomation/.bashrc
 source /Users/qaautomation/.rvm/scripts/rvm
 echo "source completed"
-bash -c "source ~/.rvm/scripts/rvm && rvm_install_on_use_flag=1 && rvm use --create 1.9.3@projectname && export > rvm.env"
+bash -c "source ~/.rvm/scripts/rvm && rvm_install_on_use_flag=1 && rvm use --create 2.0.0-p353@global && export > rvm.env"
 source rvm.env
 type rvm | head -n 1
 rvm use ruby-2.0.0-p353@global
 /Users/qaautomation/.rvm/gems/ruby-2.0.0-p353/bin/calabash-ios sim reset
+sh runmeinetui.sh clean @login1
