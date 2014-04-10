b531a6d36194a12a97d345607dd49ab9edecd845
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-04-10 14:58:20 +0100

    updated script

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -26,6 +26,9 @@ echo "source completed"
 
 bash -c "source ~/.rvm/scripts/rvm && rvm_install_on_use_flag=1 && rvm use --create 2.0.0-p353@global && export > rvm.env"
 source rvm.env
+gem install bundler
+gem list
+ls
 bundle install
 #/Users/qaautomation/.rvm/gems/ruby-2.0.0-p353/bin/
 calabash-ios sim reset
