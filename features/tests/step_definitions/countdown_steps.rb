cacee5e537af48cf25f24a02d23b164130ef5e27
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-05-07 13:46:15 +0100

    fixing one test

--- features/tests/step_definitions/countdown_steps.rb
+++ features/tests/step_definitions/countdown_steps.rb
@@ -10,6 +10,7 @@ Then(/^I verify number of days to go on home page$/) do
   @homePage.check_countdown_days_text
   expected_count=CommonMethods.new.get_countdown_days.to_s
 
+  @page.assert_wait_for_text expected_count
   res=query("#{$g_query_txt}marked:'countdown_biscuit' label",:text).include? expected_count.to_s if $g_ios
   res=query("* contentDescription:'countdown_biscuit.' *",:text).include? expected_count.to_s if $g_android
   fail("Remaining days wrong exp:#{expected_count}") if (res!=true)
