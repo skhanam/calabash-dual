e0834ad12025178fe024b03502010961bd31d9a9
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-06-26 16:11:03 +0100

    added push notification handling functionality

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 gem 'calabash-android', '0.4.20'
-gem 'calabash-cucumber', '0.9.167'
+gem 'calabash-cucumber', '0.9.169'
 gem 'multi_json', '1.7.9'
 gem 'rspec', '1.3.0'
 gem 'rubyXL'
--- features/common/support/Z_view_functions.rb
+++ features/common/support/Z_view_functions.rb
@@ -228,12 +228,4 @@ module ViewModule
     click_accessibility_label "navbarLeftButton"
   end
 
-
-  def close_whats_new_dialog
-    if element_exists("#{$g_query_txt}text:'What\\\'s new?'")
-      touch ("#{$g_query_txt}text:'OK'")
-      sleep 2
-    end
-  end
-
 end
