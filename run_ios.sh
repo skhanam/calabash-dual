61874e4360de450245e02433073d52e619e4d7cd
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-09-22 10:20:07 +0100

    using ti calabash to add calabash framework

--- run_ios.sh
+++ run_ios.sh
@@ -4,18 +4,9 @@ DATE=`date +%d-%m-%Y-%H-%M`
 
 export LC_CTYPE=en_US.UTF-8
 
-if [ "$#" -le "3" ]; then
-	echo "\n4 ARGUMENTS NEEDED"
-	echo "1) clean(clean project) or NA (for running project without cleaning"
-	echo "2) Tags selected for test run ex: @sanity or @reg"
-    echo "3) App to test ex: en_th / de / en_fc /sv / fi/ da /nb "
-    echo "4) Harware on which tests are run tablet/ phone"
-    echo "5) relative folder path where source code is located"
 
-	echo "\nsample command: \n 1) sh run_ios.sh clean @tab-sanity de tablet ../tda.tablet"
-	echo " 2) sh run_ios.sh NA @sanity da tablet ../meine.tui\n"
-	exit
-fi
+LANG=$3
+PROJ_FOLDER=$5
 
 if [ -z "$2" ] ; then
 echo "Tags not specified using @failed"
@@ -24,40 +15,94 @@ else
 tagged_test=$2
 fi
 
-PROJ_FOLDER=$5
+if [ "$#" -le "4" ]; then
+	echo "\n4 ARGUMENTS NEEDED"
+	echo "1) clean(clean project) or NA (for running project without cleaning"
+	echo "2) Tags selected for test run ex: @sanity or @reg"
+    echo "3) App to test ex: en_th / de / en_fc /sv / fi/ da /nb "
+    echo "4) Hardware on which tests are run tablet/ phone"
+    echo "5) relative folder path where source code is located"
 
-if [ $3 == "de" ] ; then
+	echo "\nSample command: \n 1) sh run_ios.sh clean @tab-sanity de tablet ../tda.tablet"
+	echo " 2) sh run_ios.sh NA @sanity da tablet ../meine.tui\n"
+	exit
+fi
+
+
+if [ $LANG == "de" ] ; then
 	TI_SCHEME="meinetui"
 	APPNAME="meineTui"
 	TESTENV='DE_MT'
 	CUCUMBER_PROFILE=de_mt_ios
 	calabash-ios sim locale en
-elif [ $3 == "en_th" ] ; then
+elif [ $LANG == "en_th" ] ; then
 	TI_SCHEME="thomson"
 	APPNAME="MyThomson"
 	TESTENV='EN_TH'
 	CUCUMBER_PROFILE=en_th_ios
 	calabash-ios sim locale en
-elif [ $3 == "en_fc" ] ; then
+elif [ $LANG == "en_fc" ] ; then
 	TI_SCHEME="firstchoice"
 	APPNAME="MyFirstChoice"
 	CUCUMBER_PROFILE=en_fc_ios
 	TESTENV='EN_FC'
 	calabash-ios sim locale en
-elif [ $3 == "sv" ] || [ $3 == "da" ] || [ $3 == "fi" ] || [ $3 == "nb" ] ; then
-	calabash-ios sim locale $3
+elif [ $LANG == "sv" ] || [ $LANG == "da" ] || [ $LANG == "fi" ] || [ $LANG == "nb" ] ; then
+	calabash-ios sim locale $LANG
 	TI_SCHEME="nordics"
 	APPNAME="MinFerie"
 	CUCUMBER_PROFILE=nor_ios
 	TESTENV='NOR'
 fi
 
-SCHEME="${APPNAME}"
-
 if [ $4 == "tablet" ] ; then
 	APPNAME=$APPNAME"Tablet"
-	SCHEME=$SCHEME"Tablet"
 fi
+
+if [ "$1" == "clean" ] ; then
+	echo "project name:"${PROJ_FOLDER}
+	echo "******** ####  Updating All Projects"
+
+	cp expect.exp ../${PROJ_FOLDER}/
+	cd ${PROJ_FOLDER}/
+	ti clean
+	/usr/local/bin/grunt
+	node releaseScripts/build.js --brand $TI_SCHEME
+	node releaseScripts/build.js --brand $TI_SCHEME -l
+	cd -
+
+  	echo ruby update_tiapp.rb $PROJ_FOLDER "${APPNAME}"
+	ruby update_tiapp.rb $PROJ_FOLDER "${APPNAME}"
+
+    cd ${PROJ_FOLDER}/
+    ti build -p ios -Y ipad -b --retina
+	expect expect.exp ${APPNAME}
+    cd -
+fi
+
+if [ "$1" == "clean" ] || [ "$1" != "ci" ] ; then
+	cp -r ${PROJ_FOLDER}/i18n/* features/test_data/
+	killall "iPhone Simulator"
+	killall Xcode
+	sleep 1
+fi
+
+#cp expect.exp ../tda.tablet/
+#cd ../tda.tablet
+#ti clean
+#ti build -p ios -Y ipad -b --retina
+#expect expect.exp
+#cd -
+
+exit
+
+cp -r ../tda.tablet/build/iphone/build/Debug-iphonesimulator/"${APPNAME}".app ios$LANG.app
+echo DEVICE_TARGET='iPad Retina (64-bit) - Simulator - iOS 7.1' OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$LANG LANG=$LANG APP_BUNDLE_PATH=./ios$LANG.app bundle exec cucumber -p $CUCUMBER_PROFILE features/ --tag @sanity312
+DEVICE_TARGET='iPad Retina (64-bit) - Simulator - iOS 7.1' OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$LANG LANG=$LANG APP_BUNDLE_PATH=./ios$LANG.app bundle exec cucumber -p $CUCUMBER_PROFILE features/ --tag @sanity312
+exit
+
+---------------------------------
+
 
 BUILD_CONFIG="Debug"
 PROJECT_PATH="features"
@@ -65,42 +110,6 @@ ARCHITECTURE_SELECTED=i386
 SCHEME="${SCHEME}-iPad"
 SCHEME_CAL="${SCHEME}-cal"
 PROJ_LOC="${PROJ_FOLDER}/build/iphone/${APPNAME}.xcodeproj"
-
-if [ "$1" == "clean" ] ; then
-	echo "project name:"${PROJ_FOLDER}
-	echo "******** ####  Updating All Projects"
-
-	cd ${PROJ_FOLDER}/
-	ti clean
-	/usr/local/bin/grunt
-	echo node releaseScripts/build.js --brand $TI_SCHEME
-	node releaseScripts/build.js --brand $TI_SCHEME
-	node releaseScripts/build.js --brand $TI_SCHEME -l
-	#node tda $TI_SCHEME
-	#node tda $TI_SCHEME -l
-echo node releaseScripts/build.js --brand $TI_SCHEME -l
-	cd -
-
-  	echo ruby update_tiapp.rb $PROJ_FOLDER "${APPNAME}"
-	ruby update_tiapp.rb $PROJ_FOLDER "${APPNAME}"
-
-    cd ${PROJ_FOLDER}/
-    titanium build --platform ios -S 7.1 -Y ipad -b
-    cd -
-
-   	killall Xcode
-   	cp ./expect.exp ${PROJ_FOLDER}
-   	cd ${PROJ_FOLDER}/
-   	echo /usr/bin/expect ./expect.exp $APPNAME $SCHEME
-   	/usr/bin/expect ./expect.exp $APPNAME $SCHEME
-   	cd -
-
-   	open -a Xcode
-   	sleep  10
-   	echo ${PROJ_LOC}
-   	open ${PROJ_LOC}
-   	sleep 20
-fi
 
 
 #Do this below step only when compile option is selected -  used for CI
@@ -140,8 +149,9 @@ if [ "$2" != "NA" ] ; then
 		APP_BUNDLE_PATH_VAR=../Debug-iphonesimulator/"${APPNAME}".app
 	fi
 
-	echo DEVICE_TARGET="iPad Retina - Simulator - iOS 7.1" OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$3 LANG=$3 BUNDLE_ID=$BUNDLE APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
-	DEVICE_TARGET="iPad - Simulator - iOS 7.1" OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$3 LANG=$3 APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
+tda.tablet/build/iphone/build/Debug-iphonesimulator/meineTuiTablet.app
+	echo DEVICE_TARGET="iPad Retina - Simulator - iOS 7.1" OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$3 LANG=$3 BUNDLE_ID=$BUNDLE APP_BUNDLE_PATH=../tda.tablet/build/iphone/build/Debug-iphonesimulator/meineTuiTablet.app bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
+	DEVICE_TARGET="iPad - Simulator - iOS 7.1" OS=ios HW=tablet TESTENV=$TESTENV SCREENSHOT_PATH=features/report/ios$3 LANG=$3 APP_BUNDLE_PATH=../tda.tablet/build/iphone/build/Debug-iphonesimulator/meineTuiTablet.app bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
 
 	#echo DEVICE_TARGET='iPhone Retina (4-inch) - Simulator - iOS 7.1' TESTENV=$TESTENV SCREENSHOT_PATH=features/report/iosscreenshots/$3 LANG=$3 BUNDLE_ID=$BUNDLE DEVICE=phone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
 	#DEVICE_TARGET='iPhone Retina (4-inch) - Simulator - iOS 7.1' TESTENV=$TESTENV SCREENSHOT_PATH=features/report/iosscreenshots/$3 LANG=$3 BUNDLE_ID=$BUNDLE DEVICE=phone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p $CUCUMBER_PROFILE features/  --tag $tagged_test  -f html -o ios-$3-report.html  -f junit -o features/report/junit/$3
