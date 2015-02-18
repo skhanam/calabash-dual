2ce577a164568dc07ab2f82f47716b15a26f82ab
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2015-02-18 11:00:15 +0000

    formatting android shell script

--- run_android.sh
+++ run_android.sh
@@ -5,19 +5,19 @@ DATE=`date +%d-%m-%Y-%H-%M`
 export LC_CTYPE=en_US.UTF-8
 
 if [ "$7" == "ci" ]; then
-echo "Running tests from CI"
-export ANDROID_HOME=$HOME/Library/android-sdk-macosx
-export PATH=$ANDROID_HOME/tools:$PATH
-export PATH=$ANDROID_HOME/platform-tools:$PATH
+	echo "Running tests from CI"
+	export ANDROID_HOME=$HOME/Library/android-sdk-macosx
+	export PATH=$ANDROID_HOME/tools:$PATH
+	export PATH=$ANDROID_HOME/platform-tools:$PATH
 fi
 
 if [ "$#" -le "4" ]; then
 	echo "\n4 ARGUMENTS NEEDED"
 	echo "1) clean(clean project) or NA (for running project without cleaning"
 	echo "2) Tags selected for test run ex: @sanity or @reg"
-    echo "3) App to test ex: en_th / de / en_fc /sv / fi/ da /nb "
-    echo "4) Hardware on which tests are run tablet/ phone"
-    echo "5) relative folder path where source code is located"
+  echo "3) App to test ex: en_th / de / en_fc /sv / fi/ da /nb "
+  echo "4) Hardware on which tests are run tablet/ phone"
+  echo "5) relative folder path where source code is located"
 
 	echo "\nSample command: \n 1) sh run_android.sh clean @sanity de phone ../tda"
 	echo " 2)sh run_android.sh NA @tab-sanity de phone ../tda"
@@ -40,12 +40,11 @@ LANG_STR=$LANG
 FILENAME="../Appfiles/android$LANG$HW.apk"
 mkdir -p ../Appfiles
 
-
 if [ -z "$2" ] ; then
-echo "Tags not specified using @failed"
-exit
+	echo "Tags not specified using @failed"
+	exit
 else
-tagged_test=$2
+	tagged_test=$2
 fi
 
 echo "TDA project location:"${PROJ_FOLDER}
@@ -54,29 +53,29 @@ if [ $LANG == "de" ] ; then
 	APK_NAME="meine TUI"
 	CUCUMBER_PROFILE=de_mt_android
 	TI_SCHEME=meinetui
-    TESTENV='DE_MT'
-    PACKAGE_ID="de.tui.meinetui"
+  TESTENV='DE_MT'
+  PACKAGE_ID="de.tui.meinetui"
 
 elif [ $LANG == "en_th" ] ; then
 	APK_NAME=MyThomson
 	CUCUMBER_PROFILE=en_th_android
 	TI_SCHEME=thomson
-    TESTENV='EN_TH'
-    PACKAGE_ID="com.thomson.mythomson"
+  TESTENV='EN_TH'
+  PACKAGE_ID="com.thomson.mythomson"
 
 elif [ $LANG == "en_fc" ] ; then
 	APK_NAME=MyFirstChoice
 	CUCUMBER_PROFILE=en_fc_android
 	TI_SCHEME=firstchoice
-    TESTENV='EN_FC'
-    PACKAGE_ID="com.firstchoice.myfirstchoice"
+  TESTENV='EN_FC'
+  PACKAGE_ID="com.firstchoice.myfirstchoice"
 
 elif [ $LANG == "sv" ] || [ $LANG == "da" ] || [ $LANG == "fi" ] || [ $LANG == "nb" ] ; then
 	TI_SCHEME="nordics"
 	APK_NAME="MinFerie"
 	CUCUMBER_PROFILE=nor_android
 	TESTENV='NOR'
-    PACKAGE_ID="com.tuitravel.minferie"
+  PACKAGE_ID="com.tuitravel.minferie"
 fi
 
 if [ $HW == "phone" ] ; then
@@ -137,20 +136,21 @@ if [ $1 == "install" ] || [ $1 == "clean" ] ; then
 
 	# Copy the required apk files
 	if [ "$7" == "ci" ]; then
-	echo cp $PROJ_FOLDER/build/android/bin/"$APK_NAME"  $FILENAME
-	cp $PROJ_FOLDER/build/android/bin/"$APK_NAME"  $FILENAME
-	calabash-android resign $FILENAME
+		echo cp $PROJ_FOLDER/build/android/bin/"$APK_NAME"  $FILENAME
+		cp $PROJ_FOLDER/build/android/bin/"$APK_NAME"  $FILENAME
+		calabash-android resign $FILENAME
 	fi
 
 	#Do not perform below steps when there are no tests selected to run
 fi
 
 if [ "$2" != "NA" ] ; then
-	rm -rf test_servers/
-	calabash-android resign $FILENAME
-	calabash-android build $FILENAME
-	adb install -r $FILENAME
-	adb install -r test_servers/*.apk
+	echo "Android tablet"
+#	rm -rf test_servers/
+#	calabash-android resign $FILENAME
+#	calabash-android build $FILENAME
+#	adb $ADB_DEVICE install -r $FILENAME
+#	adb $ADB_DEVICE install -r test_servers/*.apk
 fi
 
 if [ "$2" != "NA" ] ; then
